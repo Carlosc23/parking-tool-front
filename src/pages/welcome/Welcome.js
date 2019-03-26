94632aad0f3c735f1e68b41365e1ab860c0388a3
Carlosc23 <carlos@inegua.com>
2019-03-25 22:30:17 -0600

    feat: add initial Welcome page
    It was added the initial structure for the welcome page of the app.

---
+++ src/pages/welcome/Welcome.js
@@ -0,0 +1,20 @@
+import React, { Component }  from "react";
+//import '../styles/Welcome.css';
+export default class Welcome extends Component {
+    render() {
+      return (
+        <header>
+        <div >
+        <h1>
+          Bienvenido al Parqueo
+        </h1>
+        <p>
+            Seleccione el tipo de parqueo que necesita
+        </p>
+        <img src=".../assets/img/logo.png" />
+        </div>
+      </header>
+      );
+    }
+  }
+  
