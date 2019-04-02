04b52aab4238e3d41f5ee8227c2a1a8bf019c3b1
Carlosc23 <carlos@inegua.com>
2019-04-01 20:11:04 -0600

    feat: organize Welcome page
    Now welcome page has the correct size and fonts. Only needs the type of vehicles.

--- src/pages/welcome/Welcome.js
+++ src/pages/welcome/Welcome.js
@@ -8,10 +8,12 @@ export default class Welcome extends Component {
         <div className="frame" >
         <header>
         
-        <h1>
+        <h1 >
           Bienvenido al Parqueo
         </h1>
+        <div className="logo">
         <img src={logo} alt="logo" width="30%" height="30%" />
+        </div>
         <p>
             Seleccione el tipo de parqueo que necesita
         </p>
