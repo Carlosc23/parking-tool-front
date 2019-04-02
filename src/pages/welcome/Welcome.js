1b45371c81945da1cd46084f154d73e801121e2a
Carlosc23 <carlos@inegua.com>
2019-04-01 20:17:42 -0600

    feat: add icons for type of parking

--- src/pages/welcome/Welcome.js
+++ src/pages/welcome/Welcome.js
@@ -1,6 +1,9 @@
 import React, { Component }  from "react";
 
 import logo from '../../assets/img/logo.png';
+import normalc from '../../assets/img/normalc.PNG';
+import pregnant from '../../assets/img/pregnant.PNG';
+import specialn from '../../assets/img/specialn.PNG';
 import '../../styles/Welcome.css'
 export default class Welcome extends Component {
     render() {
@@ -17,6 +20,11 @@ export default class Welcome extends Component {
         <p>
             Seleccione el tipo de parqueo que necesita
         </p>
+        <div className="typeParking">
+        <img src={normalc} alt="normalc" width="30%" height="30%" />
+        <img src={pregnant} alt="pregnant" width="30%" height="30%" />
+        <img src={specialn} alt="specialn" width="30%" height="30%" />
+        </div>
         
       </header>
       </div>
